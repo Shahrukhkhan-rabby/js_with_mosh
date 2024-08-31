e27d1f3a4fc2af1cf7a51fc51753875c018902de
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-31 18:17:40 +0600

    gutton

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ document.querySelector("work_button").addEventListener("mouseout", function () {
     this.style.backgroundColor = "#1e1e1e";
     this.style.color = "#ffffff";
 });
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display='flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display='none'   
+}
+
